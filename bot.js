88956bd9f1db299bf5b4a3be5e78a6f45fcd2872
atskotzen <atsaudio@gmail.com>
2024-04-02 05:35:40 -0300

    exclui codigo inutil

--- bot.js
+++ bot.js
@@ -1,24 +1,12 @@
 require('dotenv').config();
 const { Telegraf } = require("telegraf");
 const { handleStart, handleText, handlePhoto } = require("./botInteractions");
-const { setSafState, getSafState } = require('./safStateManager');
 const BOT_TOKEN = process.env.BOT_TOKEN; // Acessando o BOT_TOKEN diretamente do ambiente
 const bot = new Telegraf(BOT_TOKEN);
 
 bot.start(handleStart);
 bot.on("text", handleText);
 bot.on("photo", handlePhoto);
-
-// bot.on("poll_answer", async (ctx) => {
-//   const answerText = pollOptions[ctx.update.poll_answer.option_ids[0]];  
-//   console.log("================AGROFOREST_DAO====================");
-//   console.log("Pergunta: ", question);
-//   console.log("Resposta:", answerText);
-//   console.log("Telegram username: ", ctx.update.poll_answer.user.username);
-//   console.log("Telegram Chat ID:", CHAT_ID);
-//   console.log("User ID: ", ctx.update.poll_answer.user.id);
-//   console.log("================TELEGRAM BOT=================");
-// });
 
 bot.launch();
 
